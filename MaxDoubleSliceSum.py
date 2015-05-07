e752046d5156045153797eded0ecae63d4aa19ab
wzy0791 <wzy0791@users.noreply.github.com>
2015-05-07 13:44:17 -0600

    Update MaxDoubleSliceSum.py

--- MaxDoubleSliceSum.py
+++ MaxDoubleSliceSum.py
@@ -1,16 +1,16 @@
 def solution(A):
-    y_ending_here = [0] * len(A)
-    y_starting_here = [0] * len(A)
+    max_before_here = [0] * len(A)
+    max_after_here = [0] * len(A)
      
     for idx in range(2, len(A)-1): # y_idx goes from 2 to N-2
-        y_ending_here[idx] = max(0, y_ending_here[idx-1] + A[idx-1])
+        max_before_here[idx] = max(0, max_before_here[idx-1] + A[idx-1])
      
     for idx in reversed(range(1, len(A)-2)): # y_idx goes from N-3 to 1
-        y_starting_here[idx] = max(0, y_starting_here[idx+1] + A[idx+1])
+        max_after_here[idx] = max(0, max_after_here[idx+1] + A[idx+1])
      
     max_double_slice = 0
      
     for idx in range(1, len(A)-1):
-        max_double_slice = max(max_double_slice, y_starting_here[idx] + y_ending_here[idx])
+        max_double_slice = max(max_double_slice, max_before_here[idx] + max_after_here[idx])
          
     return max_double_slice
