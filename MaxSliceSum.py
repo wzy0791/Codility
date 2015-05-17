69e1223dfba0a4cc9647cb402e2abde5ffb5cca3
wzy0791 <wzy0791@users.noreply.github.com>
2015-05-16 22:49:17 -0600

    Update MaxSliceSum.py

--- MaxSliceSum.py
+++ MaxSliceSum.py
@@ -1,11 +1,8 @@
 def solution(A):
     # write your code in Python 2.7
-    max_slice = max(A)
-    max_ending = 0
-    for x in A :
-        if (max_ending + x) < 0 :
-            max_ending = 0
-        else :
-            max_ending += x
-            max_slice = max(max_slice, max_ending)
-    return max_slice
+    max_sum = max_end_at = A[0]
+    for x in A[1:]:
+        max_end_at = max(max_end_at+x, x)
+        max_sum = max(max_sum, max_end_at)
+        
+    return max_sum
