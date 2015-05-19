448bcd00a8ac6a6de9b24d56f1a09585672c5a4a
wzy0791 <wzy0791@users.noreply.github.com>
2015-05-19 16:16:32 -0600

    Create MissingInteger.cpp

---
+++ MissingInteger.cpp
@@ -0,0 +1,16 @@
+int solution(vector<int> &A) {
+    // write your code in C++11
+    int n = A.size();
+    vector<int> count(n+1, 0);
+    for (int x : A) {
+        if (x > 0 && x <= n) {
+            count[x]++;
+        }
+    }
+    
+    for (int i=1; i<n+1; i++) {
+        if (count[i] == 0) return i;
+    }
+    
+    return n+1;
+}
