4e3ba20dcf6bc8d2f54e55b78f61ce300d277bee
wzy0791 <wzy0791@users.noreply.github.com>
2015-06-04 23:56:20 -0600

    Update and rename CountDistinctSlices.py to CountDistinctSlices.cpp

---
+++ CountDistinctSlices.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+
+int solution(int M, vector<int> &A) {
+    // write your code in C++11
+    int count = 0;
+    vector<bool> used(M+1, false);
+    int i = 0, j = 0;
+    int n = A.size();
+    while (j<n) {
+        if (used[A[j]] == false) { // moving j
+            used[A[j]] = true;
+            count += j-i+1; // key: how to calculate the number of slices
+            if (count > 1000000000) return 1000000000;
+            j++;
+        } else { // moving i
+            used[A[i]] = false;
+            i++;
+        }       
+    }
+    return count;
+}
